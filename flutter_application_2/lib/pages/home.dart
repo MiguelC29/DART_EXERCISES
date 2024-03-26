db130771a8ba83f1f1fe08c942de50bb6eba254d
MiguelC29 <miguelito28c@gmail.com>
2024-03-26 12:03:37 -0500

    añadiendo botones y estilos al card

--- flutter_application_2/lib/pages/home.dart
+++ flutter_application_2/lib/pages/home.dart
@@ -6,7 +6,7 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.blueGrey.shade100,
+      backgroundColor: Colors.blueGrey.shade200,
       appBar: AppBar(
         elevation: 0,
         backgroundColor: Colors.blueGrey.shade100,
@@ -22,28 +22,65 @@ class HomePage extends StatelessWidget {
                 color: Colors.black87,
               ),
             ),
-            Expanded(
-              child: Card(
-                child: ListView(
-                  shrinkWrap: true,
-                  padding: const EdgeInsets.all(15),
-                  children: [
-                    Container(
-                      height: 50,
-                      margin: const EdgeInsets.only(top: 20, bottom: 27),
-                      child: const Center(
-                        child: Text('OUR APP',
-                        style: TextStyle(
-                          fontWeight: FontWeight.w600,
-                          fontSize: 35,
-                        ),
-                        ),
+            // Expanded(
+              //child: 
+            Card(
+              margin: const EdgeInsets.all(10),
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(12)
+              ),
+              color: Colors.green.shade200,
+              child: ListView(
+                shrinkWrap: true,
+                padding: const EdgeInsets.all(15),
+                children: [
+                  Container(
+                    height: 50,
+                    margin: const EdgeInsets.only(top: 20, bottom: 27),
+                    child: const Center(
+                      child: Text('OUR APP',
+                      style: TextStyle(
+                        fontWeight: FontWeight.w600,
+                        fontSize: 35,
                       ),
-                    )
-                  ],
-                ),
+                      ),
+                    ),
+                    decoration: BoxDecoration(
+                      border: Border.all(color: Colors.black),
+                      gradient: LinearGradient(
+                        begin: Alignment.topRight,
+                        end: Alignment.topLeft,
+                        colors: [
+                          Colors.yellow.shade300,
+                          Colors.cyan.shade300,
+                        ]
+                      )
+                    ),
+                  ),
+                  OutlinedButton(onPressed: (){}, 
+                    child: const Text('Botón'),
+                    style: OutlinedButton.styleFrom(
+                      // backgroundColor: Colors.pink,
+                      // backgroundColor: Color.fromARGB(255, 10, 11, 19),
+                      backgroundColor: Colors.orange.shade400,
+                      foregroundColor: Colors.white,
+                      elevation: 5,
+                      side: const BorderSide( width: 2, color: Colors.black38),
+                    ),
+                  ),
+                  OutlinedButton(onPressed: (){},
+                    child: const Text('Botón'),
+                    style: OutlinedButton.styleFrom(
+                      backgroundColor: Colors.red.shade400,
+                      foregroundColor: Colors.white,
+                      elevation: 5,
+                      side: const BorderSide( width: 2, color: Colors.black38),
+                    ),
+                  ),
+                ],
               ),
             ),
+            // ),
           ],
         ),
       ),
